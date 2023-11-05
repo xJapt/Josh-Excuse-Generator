22f27b53fd61a7cd583e7bebee5a044ee01201e1
xJapt <149015114+xJapt@users.noreply.github.com>
2023-11-05 22:35:51 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The jogger", "The thief", "The alien", "The president"];
+  let action = ["destroyed", "ate", "threw away"];
+  let what = ["my keys", "my computer", "my wallet", "my bed"];
+  let when = [
+    "during the afternoon",
+    "before bed",
+    "after breakfast",
+    "right on time"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
